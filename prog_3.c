52d134527de7a13b1ce24dd85899dc92e6dcc60f
Firoj Madhikarmi <madhikarmifiroj@gmail.com>
2026-02-10 15:57:00 +0545

    feat: add linked list creation and forward traversal

--- prog_3.c
+++ prog_3.c
@@ -12,3 +12,23 @@ typedef struct Node {
 int main(void) {
     return 0;
 }
+// ... includes and Node as above ...
+
+Node *createNode(int data) { /* as in final code */ }
+void insertAtEnd(Node **head, int data) { /* as in final code */ }
+void printList(Node *head) { /* as in final code */ }
+void freeList(Node *head) { /* as in final code */ }
+
+int main(void) {
+    Node *head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+
+    printList(head);   // Forward only
+
+    freeList(head);
+    return 0;
+}
