808b49043cfa4a7ea005f766b64737feb7d0013d
Firoj Madhikarmi <madhikarmifiroj@gmail.com>
2026-02-10 16:00:27 +0545

    feat: implement recursive reversal traversal for linked list

--- prog_3.c
+++ prog_3.c
@@ -9,26 +9,86 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-int main(void) {
-    return 0;
+// Create a new node with given data
+Node *createNode(int data) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
-// ... includes and Node as above ...
 
-Node *createNode(int data) { /* as in final code */ }
-void insertAtEnd(Node **head, int data) { /* as in final code */ }
-void printList(Node *head) { /* as in final code */ }
-void freeList(Node *head) { /* as in final code */ }
+// Insert a new node at the end of the list
+void insertAtEnd(Node **head, int data) {
+    Node *newNode = createNode(data);
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node *temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Forward traversal: print list from head to tail
+void printList(Node *head) {
+    Node *temp = head;
+    printf("Forward traversal: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Reverse traversal using recursion:
+// visits nodes from tail back to head without changing links
 void printListReverse(Node *head) {
-    if (head == NULL) return;
+    if (head == NULL) {
+        return;
+    }
+    // First go to the end of the list
     printListReverse(head->next);
+    // Then print on the way back (reverse order)
     printf("%d ", head->data);
+}
+
+// Free all nodes in the list
+void freeList(Node *head) {
+    Node *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
 }
 
 int main(void) {
     Node *head = NULL;
+
+    // Build a simple list: 10 -> 20 -> 30 -> 40
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+
+    // Show normal (forward) traversal
     printList(head);
+
+    // Show reverse traversal (without modifying the list)
     printf("Reverse traversal: ");
     printListReverse(head);
     printf("\n");
+
+    // Clean up memory
+    freeList(head);
+
     return 0;
 }
