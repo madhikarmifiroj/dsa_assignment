1e7615d18c3bfcbce05f7b8b1bec337f19ad6a1e
Firoj Madhikarmi <madhikarmifiroj@gmail.com>
2026-02-10 15:56:37 +0545

    feat: define singly linked list node for reverse traversal

---
+++ prog_3.c
@@ -0,0 +1,14 @@
+// Program 3: Reverse traversal in a singly linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node definition for singly linked list
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+int main(void) {
+    return 0;
+}
